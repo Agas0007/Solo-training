6a489162723c57e67f20ca85a84bfcd0cf670c63
Andrey Babaryka <BabarykaSES@yandex.ru>
2019-11-19 22:22:57 +0300

    второй день

--- lesson 1/js/main.js
+++ lesson 1/js/main.js
@@ -1,21 +1,31 @@
-'use sctrict';
-let arr = {
-    a: 1,
-    b:2,
-    c:3
-};
-//console.log(arr.c);
-let b =  {
-    a: 2,
-    b:3,
-    c:4
-};
+// 'use sctrict';
+// let arr = {
+//     a: 1,
+//     b:2,
+//     c:3
+// };
+// //console.log(arr.c);
+// let b =  {
+//     a: 2,
+//     b:3,
+//     c:4
+// };
 
-let ret = function(){
-     if ( arr != b){
-        console.log(arr);
-    }else{
-        console.log(b);
-    }
+// let ret = function(){
+//      if ( arr != b){
+//         console.log(arr);
+//     }else{
+//         console.log(b);
+//     }
+// };
+//  ret();
+
+let arr = {
+    a: 'Мир',
+    b: ' Наш ',
+    c: 'c тобой',
+    d: 'к нам лицом'
 };
- ret();
+ console.log(arr.b + ' ' + arr.a + ' ' + arr.d);
+ 
+
